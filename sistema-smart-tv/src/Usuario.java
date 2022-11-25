718f7df371b5f380ceb58d718dcdd2ef91dad7e8
Sergio Rubens de Campos Cruz <sergio_ru@hotmail.com>
2022-11-25 01:20:46 -0300

    teste

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,18 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+        System.out.println();
+
+        smartTv.ligar();
+        
+
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,14 +1,5 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        int numero = 5;
-        System.out.println(numero);
-        numero = -numero;
-
-        System.out.println(numero);
-        numero = - numero;
-
-        System.out.println(numero);
-        System.out.println("teste");
-        System.out.println("teste2");
+      
     }
 }
